4f1253b5242aa140ed6c4bfa337c3ea58d3d7280
DenisBaburin <denis.v.baburin@gmail.com>
2023-09-17 21:53:38 +1100

    learning nextjs

--- next-training/client/src/app/page.tsx
+++ next-training/client/src/app/page.tsx
@@ -1,14 +1,24 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import { Icon } from "@mui/material";
-import DeleteIcon from "@mui/icons-material/Delete";
+import Icon from "@mui/material/Icon";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import { green } from "@mui/material/colors";
 
 export default function Home() {
   return (
     <main>
+      <link
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        rel="stylesheet"
+      ></link>
       <div>
-        <svg data-testid="DeleteIcon"></svg>
-        <Icon></Icon>
+        <Icon>star</Icon>
+        <Box>
+          <Icon>add_circle</Icon>
+          <Icon color="primary">add_circle</Icon>
+          <Icon sx={{ color: green[500] }}>add_circle</Icon>
+          <Icon fontSize="small">add_circle</Icon>
+          <Icon sx={{ fontSize: 30 }}>add_circle</Icon>
+        </Box>
       </div>
     </main>
   );
