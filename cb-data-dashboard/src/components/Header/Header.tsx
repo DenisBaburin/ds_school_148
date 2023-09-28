493e128a2db2ff7a70af89452f44984fd08d7b7f
DenisBaburin <denis.v.baburin@gmail.com>
2023-09-28 19:23:19 +1100

    created some small app

--- cb-data-dashboard/src/components/Header/Header.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-const Header = (props: Props) => {
-  return <h1>Header</h1>;
-};
-
-export default Header;
--- react_mui_training/src/App.tsx
+++ react_mui_training/src/App.tsx
@@ -2,41 +2,131 @@ import { useState } from "react";
 import "./App.css";
 import UserForm from "./components/dialog/UserForm";
 import Table from "./components/table";
-import { Button, Paper, TextField } from "@mui/material";
+import { Box, Button, Icon, Paper, Stack, TextField } from "@mui/material";
+import DoneIcon from "@mui/icons-material/Done";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 
 function App() {
-  const [users, setUser] = useState(["Timofey", "Denis"]);
-  const [newUser, setNewUser] = useState("");
-  const addUser = () => {
-    setUser([...users, newUser]);
-    console.log(users);
+  // const [users, setUser] = useState(["Timofey", "Denis"]);
+  // const [newUser, setNewUser] = useState("");
+  // const addUser = () => {
+  //   setUser([...users, newUser]);
+  //   console.log(users);
+  // };
+  // const handleDeleteUser = (index: number) => {
+  //   const userList = [...users];
+  //   userList.splice(index, 1);
+  //   setUser(userList);
+  //   //console.log(users);
+  // };
+  // return (
+  //   <div>
+  //     <Button onClick={addUser}>Нажми меня</Button>
+  //     <TextField onChange={(event) => setNewUser(event.target.value)} />
+  //     {users.map((user, index) => (
+  //       <Paper
+  //         onClick={() => handleDeleteUser(index)}
+  //         sx={{
+  //           margin: "2px",
+  //           backgroundColor: "greenyellow",
+  //           color: "blue",
+  //           display: "flex",
+  //           justifyContent: "end",
+  //         }}
+  //       >
+  //         {user}
+  //       </Paper>
+  //     ))}
+  //   </div>
+  // );
+  const initialNotes = [
+    { text: "Встать в 6:30", status: true },
+    { text: "Умыться и покушать", status: false },
+  ];
+
+  const [notes, setNote] = useState(initialNotes);
+  const [newNote, setNewNote] = useState("");
+  const noteField = document.getElementById("note");
+
+  const addNote = () => {
+    setNote([...notes, { text: newNote, status: false }]);
+    setNewNote("");
+    noteField.value = "";
   };
-  const handleDeleteUser = (index: number) => {
-    const userList = [...users];
-    userList.splice(index, 1);
-    setUser(userList);
-    //console.log(users);
+
+  const handleStatusChange = (index: number) => {
+    const notesList = [...notes];
+    notesList[index].status = !notesList[index].status;
+    setNote(notesList);
+    //console.log(notesList);
+  };
+
+  const handleNoteDelete = (index: number) => {
+    const notesList = [...notes];
+    notesList.splice(index, 1);
+    setNote(notesList);
+    console.log(notesList);
   };
 
   return (
-    <div>
-      <Button onClick={addUser}>Нажми меня</Button>
-      <TextField onChange={(event) => setNewUser(event.target.value)} />
-      {users.map((user, index) => (
-        <Paper
-          onClick={() => handleDeleteUser(index)}
+    <Stack>
+      <Stack sx={{ minWidth: "440px" }}>
+        <TextField
+          placeholder="Добавьте заметку"
+          onChange={(event) => setNewNote(event.target.value)}
+          id="note"
+        ></TextField>
+      </Stack>
+      <Stack>
+        <Button
           sx={{
-            margin: "2px",
-            backgroundColor: "greenyellow",
-            color: "blue",
+            backgroundColor: "beige",
+            border: "solid",
+          }}
+          onClick={newNote != "" ? addNote : () => alert("добавьте заметку")}
+        >
+          Добавить заметку
+        </Button>
+      </Stack>
+      {notes.map((note, index) => (
+        <Box
+          sx={{
             display: "flex",
-            justifyContent: "end",
+            justifyContent: "space-between",
+            gap: "2px",
+            alignItems: "center",
+            width: "500px",
+            margin: "2px",
           }}
         >
-          {user}
-        </Paper>
+          <Paper
+            sx={{
+              width: "440px",
+              lineHeight: "2",
+              border: "solid",
+              color: "#1976d2",
+              textDecorationLine: notes[index].status ? "line-through" : "",
+            }}
+          >
+            {note.text}
+          </Paper>
+          <Button
+            variant="contained"
+            color={notes[index].status ? "success" : "error"}
+            onClick={() => handleStatusChange(index)}
+          >
+            <DoneIcon />
+          </Button>
+          <Button
+            variant="contained"
+            color="error"
+            onClick={() => handleNoteDelete(index)}
+          >
+            <DeleteForeverIcon />
+          </Button>
+        </Box>
       ))}
-    </div>
+    </Stack>
   );
 }
 
