1fac9b0180b836123a65e5411d992ce2a60cf34c
DenisBaburin <denis.v.baburin@gmail.com>
2023-09-29 21:32:31 +1100

    continue EDA_4

---
+++ JS training/03-theory/02-strings.js
@@ -0,0 +1,30 @@
+const firstName = "Denis";
+const age = "17";
+
+// const old = `My name is ${name} and my age is ${age} and I ${
+//   age > 18 ? "can drive" : "can't drive"
+// }`;
+
+// console.log(old);
+
+//console.log(Math.random());
+
+// getRandom = (min, max) => {
+//   return +(Math.random() * (max - min + 1) + min).toFixed(2);
+// };
+
+// console.log(getRandom(10, 100));
+
+// console.log(firstName.length);
+// console.log(firstName.toUpperCase());
+// console.log(firstName.toLowerCase());
+// console.log(firstName.charAt(2));
+// console.log(firstName.indexOf("e"));
+// console.log(firstName.toLowerCase().startsWith("den"));
+// console.log(firstName.toLowerCase().endsWith("is"));
+// console.log(firstName.repeat(5));
+
+// const password = "     pppp    pp p";
+// console.log(password.trim());
+
+const isPrimeNumber = (num) => {};
---
+++ JS training/03-theory/03-functions.js
@@ -0,0 +1,53 @@
+// function greet(name) {
+//   console.log("Hello - " + name);
+// }
+
+// greet("Denis");
+
+// const greet2 = function (name) {
+//   console.log("2 Hello - " + name);
+// };
+
+// greet2("Denis");
+
+// // setTimeout(function () {
+// //   greet("Vladilen2");
+// // }, 1500);
+
+// let counter = 0;
+
+// const interval = setInterval(function () {
+//   if (counter === 5) {
+//     clearInterval(interval);
+//   } else {
+//     console.log(++counter);
+//   }
+// }, 1000);
+
+// function greet(name) {
+//   console.log("Hello - " + name);
+// }
+
+// const arrow = (name) => {
+//   return console.log("Hello - " + name);
+// };
+
+// const arrow2 = (name) => console.log("Hello - " + name);
+
+// arrow2("Denis");
+
+// const sum = (a = 1, b = 1) => a + b;
+
+// console.log(sum());
+
+function sumAll(...numbers) {
+  //   let res = 0;
+
+  //   for (let num of numbers) {
+  //     res += num;
+  //   }
+
+  return numbers.reduce((acc, cur) => acc + cur, 0);
+}
+
+console.log(sumAll(1, 2, 3, 3));
