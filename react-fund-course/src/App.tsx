b659f5c05151209e48197b0f0aab919599f8247b
DenisBaburin <denis.v.baburin@gmail.com>
2023-10-08 22:07:27 +1100

    continue React learning

--- react-fund-course/src/App.tsx
+++ react-fund-course/src/App.tsx
@@ -1,5 +1,25 @@
+import CreatProduct from "./components/CreatProduct";
+import ErrorMessage from "./components/ErrorMessage";
+import Loader from "./components/Loader";
+import Modal from "./components/Modal";
+import Product from "./components/Product";
+import { useRoducts } from "./hooks/products";
+
 function App() {
-  return <h1>Hello react</h1>;
+  const { loading, error, products } = useRoducts();
+
+  return (
+    <div className="container mx-auto max-w-2xl pt-5">
+      {error && <ErrorMessage error={error} />}
+      {loading && <Loader />}
+      {products.map((product) => (
+        <Product product={product} key={product.id} />
+      ))}
+      <Modal title="Create new product">
+        <CreatProduct />
+      </Modal>
+    </div>
+  );
 }
 
 export default App;
---
+++ react-fund-course/src/hooks/products.ts
@@ -0,0 +1,31 @@
+import axios, { AxiosError } from "axios";
+import { useState, useEffect } from "react";
+import { IProduct } from "../models";
+
+export function useRoducts() {
+  const [products, setProducts] = useState<IProduct[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  async function fetchProducts() {
+    try {
+      setError("");
+      setLoading(true);
+      const response = axios.get<IProduct[]>(
+        "https://fakestoreapi.com/products?limit=5"
+      );
+      setProducts((await response).data);
+      setLoading(false);
+    } catch (e: unknown) {
+      const error = e as AxiosError;
+      setLoading(false);
+      setError(error.message);
+    }
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  return { products, error, loading };
+}
