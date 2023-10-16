830d4fc200964fab450871817a7edf56ff5e8cba
DenisBaburin <denis.v.baburin@gmail.com>
2023-10-16 23:59:36 +1100

    continue eda-4 and react

--- react-projects/react-MUI-project/src/components/UserMenu.tsx
+++ react-projects/react-MUI-project/src/components/UserMenu.tsx
@@ -2,57 +2,20 @@ import { Delete } from "@mui/icons-material";
 import EditIcon from "@mui/icons-material/Edit";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { IconButton } from "@mui/material";
-import Menu, { MenuProps } from "@mui/material/Menu";
+import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
-import { alpha, styled } from "@mui/material/styles";
 import * as React from "react";
-import { UserProps } from "./UsersTableBody";
 import useUsers from "../hooks/users";
+import { UserProps } from "./UsersTableBody";
 
-const StyledMenu = styled((props: MenuProps) => (
-  <Menu
-    elevation={0}
-    anchorOrigin={{
-      vertical: "bottom",
-      horizontal: "right",
-    }}
-    transformOrigin={{
-      vertical: "top",
-      horizontal: "right",
-    }}
-    {...props}
-  />
-))(({ theme }) => ({
-  "& .MuiPaper-root": {
-    borderRadius: 6,
-    marginTop: theme.spacing(1),
-    minWidth: 180,
-    color:
-      theme.palette.mode === "light"
-        ? "rgb(55, 65, 81)"
-        : theme.palette.grey[300],
-    boxShadow:
-      "rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px",
-    "& .MuiMenu-list": {
-      padding: "4px 0",
-    },
-    "& .MuiMenuItem-root": {
-      "& .MuiSvgIcon-root": {
-        fontSize: 18,
-        color: theme.palette.text.secondary,
-        marginRight: theme.spacing(1.5),
-      },
-      "&:active": {
-        backgroundColor: alpha(
-          theme.palette.primary.main,
-          theme.palette.action.selectedOpacity
-        ),
-      },
-    },
-  },
-}));
+interface UserMenuProps {
+  onDelete: (index: number) => void;
+}
 
-export default function CustomizedMenus({ index }: UserProps) {
+export default function CustomizedMenus(
+  { index }: UserProps,
+  {}: UserMenuProps
+) {
   const { users, setUsers } = useUsers();
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
@@ -66,8 +29,6 @@ export default function CustomizedMenus({ index }: UserProps) {
   const handleCloseDelete = (index: number) => {
     users.splice(index, 1);
     setUsers(users);
-    console.log(index);
-    console.log(users);
     setAnchorEl(null);
   };
 
@@ -76,15 +37,7 @@ export default function CustomizedMenus({ index }: UserProps) {
       <IconButton id="demo-customized-button" onClick={handleClick}>
         <MoreVertIcon />
       </IconButton>
-      <StyledMenu
-        id="demo-customized-menu"
-        MenuListProps={{
-          "aria-labelledby": "demo-customized-button",
-        }}
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-      >
+      <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
         <MenuItem onClick={handleClose} disableRipple>
           <EditIcon />
           Edit
@@ -93,7 +46,7 @@ export default function CustomizedMenus({ index }: UserProps) {
           <Delete />
           Delete
         </MenuItem>
-      </StyledMenu>
+      </Menu>
     </div>
   );
 }
