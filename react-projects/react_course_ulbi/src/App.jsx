0a3a4dfeac685290c5a77e7d25cff452b51ac66d
DenisBaburin <denis.v.baburin@gmail.com>
2023-10-23 22:19:26 +1100

    new react course

--- react-projects/react_course_ulbi/src/App.jsx
+++ react-projects/react_course_ulbi/src/App.jsx
@@ -1,15 +1,34 @@
 import { useState } from "react";
-import Counter from "./components/Counter";
+import "./components/styles/App.css";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
-  const [value, setValue] = useState("Какой-то текст");
+  const [posts, setPosts] = useState([
+    { id: 1, title: "Javascript", body: "Description" },
+    { id: 2, title: "Javascript 2", body: "Description" },
+    { id: 3, title: "Javascript 3", body: "Description" },
+  ]);
+  const [title, setTitle] = useState("");
+  const addNewPost = (e) => {
+    e.preventDefault();
+  };
 
   return (
-    <>
-      <div className="App">
-        <Counter />
-      </div>
-    </>
+    <div className="App">
+      <form action="">
+        <MyInput
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          type="text"
+          placeholder="Название поста"
+        />
+        <MyInput type="text" placeholder="Описание поста" />
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
+      </form>
+      <PostList posts={posts} title={"Список постов 1"} />
+    </div>
   );
 }
 
